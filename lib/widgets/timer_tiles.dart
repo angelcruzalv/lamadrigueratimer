ac44a3ac85bcc2510f55040c9020443fb2be9bf5
Angel Cruz (Windows) <37281529+angelcruzalv@users.noreply.github.com>
2021-03-31 19:06:35 -0600

    listview builder with streambuilder

--- lib/widgets/timer_tiles.dart
+++ lib/widgets/timer_tiles.dart
@@ -1,10 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:stop_watch_timer/stop_watch_timer.dart';
 
-class TimerTiles extends StatelessWidget {
-  const TimerTiles({Key key}) : super(key: key);
+class TimerTiles extends StatefulWidget {
+  TimerTiles({Key key}) : super(key: key);
+
+  @override
+  _TimerTilesState createState() => _TimerTilesState();
+}
+
+class _TimerTilesState extends State<TimerTiles> {
+  StopWatchTimer _stopWatchTimer = StopWatchTimer();
+  final _isHours = true;
+
+  @override
+  void dispose() {
+    super.dispose();
+    _stopWatchTimer.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return ListView();
+    return ListView.builder(
+      itemCount: 12,
+      itemBuilder: (_, i) => Card(
+          child: ListTile(
+        leading: Text((i + 1).toString()),
+        title: StreamBuilder<int>(
+            stream: _stopWatchTimer.rawTime,
+            builder: (context, snapshot) {
+              final value = snapshot.data;
+              final displayTime =
+                  StopWatchTimer.getDisplayTime(value, hours: _isHours);
+              return Text(displayTime);
+            }),
+      )),
+    );
   }
 }
